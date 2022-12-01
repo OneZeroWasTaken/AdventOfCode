7f3f1671eab6c10c9ceda43976405352df98becf
Erik <erik.g.eriksson1@gmail.com>
2022-12-01 11:19:05 +0100

    Day 1

---
+++ 2022/Day1/One.hs
@@ -0,0 +1,29 @@
+module One where
+
+import           Data.List
+
+main :: IO ()
+main = do
+  content <- readFile "input.txt"
+  let l  = lines content
+  let il = parseToIntLists l []
+  let mc = maxCalories il
+  putStrLn $ "Max calories: " ++ show mc
+
+  let top = topMax 3 $ map sum il
+  putStrLn $ "Top 3 calories: " ++ show (sum top)
+  return ()
+
+topMax :: Int -> [Int] -> [Int]
+topMax 0 _  = []
+topMax n il = maxim : topMax (n - 1) (delete maxim il)
+  where maxim = maximum il
+
+maxCalories :: [[Int]] -> Int
+maxCalories = maximum . map sum
+
+parseToIntLists :: [String] -> [Int] -> [[Int]]
+parseToIntLists [] acc = [acc]
+parseToIntLists (s : ss) acc | s == ""   = acc : parseToIntLists ss []
+                             | otherwise = parseToIntLists ss (c : acc)
+  where c = (read :: String -> Int) s
