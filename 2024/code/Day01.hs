0491e85d8242835999559b3991ca3d1347726fbd
Erik <erik.g.eriksson1@gmail.com>
2024-12-01 09:36:20 +0100

    Day 1 2024

--- 2024/code/Day01.hs
+++ 2024/code/Day01.hs
@@ -1,7 +1,22 @@
+{-# OPTIONS_GHC -Wno-incomplete-uni-patterns #-}
 module Day01 where
 
+import           Data.List
 
 main :: IO ()
 main = do
   l <- lines <$> readFile "inputs/input01.txt"
-  putStrLn $ "Part 1: "
+  let (a', b') = unzip $ map parse l
+      (a , b ) = (sort a', sort b')
+
+  putStrLn $ "Part 1: " ++ show (sum $ zipWith (curry distance) a b)
+  putStrLn $ "Part 2: " ++ show (sum $ map (`similarity` b) a)
+
+similarity :: Int -> [Int] -> Int
+similarity n = (* n) . length . filter (== n)
+
+distance :: (Int, Int) -> Int
+distance (a, b) = abs (a - b)
+
+parse :: String -> (Int, Int)
+parse s = (l, r) where [l, r] = map read . words $ s
