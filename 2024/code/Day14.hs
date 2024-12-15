2cc66889680bea9685a2ec8d2c024428d3755d79
Erik <erik.g.eriksson1@gmail.com>
2024-12-15 22:31:38 +0100

    Day 14

---
+++ 2024/code/Day14.hs
@@ -0,0 +1,104 @@
+{-# OPTIONS_GHC -Wno-incomplete-uni-patterns #-}
+{-# OPTIONS_GHC -Wno-type-defaults #-}
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Avoid reverse" #-}
+module Day14 where
+
+import           Data.List.Split
+import           Data.List
+import           Data.ByteString.Internal
+import           Codec.Compression.GZip         ( compress )
+import qualified Data.ByteString.Lazy          as BL
+
+data Robot = Robot (Int, Int) (Int, Int)
+ deriving (Eq, Show)
+
+main :: IO ()
+main = do
+  l <- lines <$> readFile "inputs/input14.txt"
+
+  let robs    = map parse l
+      hundred = map ((!! 100) . iterate (teleport . step)) robs
+  putStrLn $ "Part 1: " ++ show (multiplyQuadrants hundred)
+
+  let ps = map mapPos (allIterations robs)
+      bs = map positionsToBoolList ps
+
+  -- Accidentally implemented 3 solutions to part 2 because I had off by one error in all of them first :)
+  putStrLn $ "Part 2: " ++ show
+    (snd . maximum . sort . zip (map entropy bs) $ [1 ..])
+  putStrLn $ "Part 2: " ++ show
+    (snd . minimum $ zip (map approxKolmogorov bs) [1 ..])
+  putStrLn $ "Part 2: " ++ show
+    (head $ filter (or . fst) (zip (map (map and . divvy 20 1) bs) [1 ..]))
+  printR $ map ((!! 6377) . iterate (teleport . step)) robs
+
+positionsToBoolList :: [(Int, Int)] -> [Bool]
+positionsToBoolList ps =
+  [ (x, y) `elem` ps | x <- [0 .. fst bounds - 1], y <- [0 .. snd bounds - 1] ]
+
+allIterations :: [Robot] -> [[Robot]]
+allIterations rs =
+  scanl (\r' _ -> map (teleport . step) r') rs [0 .. uncurry (*) bounds]
+
+multiplyQuadrants :: [Robot] -> Int
+multiplyQuadrants rs = product
+  $ map (\q -> length $ filter (inQuadrant q) rs) quadrants
+ where
+  quadrants :: [[(Int, Int)]]
+  quadrants =
+    [ [ (x, y) | x <- [0 .. 49], y <- [0 .. 50] ]
+    , [ (x, y) | x <- [51 .. 100], y <- [0 .. 50] ]
+    , [ (x, y) | x <- [0 .. 49], y <- [52 .. 102] ]
+    , [ (x, y) | x <- [51 .. 100], y <- [52 .. 102] ]
+    ]
+
+approxKolmogorov :: [Bool] -> Int
+approxKolmogorov bools =
+  let serialized = BL.pack (map (\b -> if b then c2w '.' else c2w '#') bools)
+  in  fromEnum $ BL.length (compress serialized)
+
+entropy :: [Bool] -> Double
+entropy bools =
+  let total     = fromIntegral (length bools) :: Double
+      countTrue = fromIntegral (length (filter id bools)) :: Double
+      p1        = countTrue / total
+      p0        = 1 - p1
+      entropyTerm p = if p == 0 then 0 else -(p * logBase 2 p)
+  in  entropyTerm p1 + entropyTerm p0
+
+inQuadrant :: [(Int, Int)] -> Robot -> Bool
+inQuadrant ps (Robot p _) = p `elem` ps
+
+teleport :: Robot -> Robot
+teleport r@(Robot (x, y) s)
+  | x < 0           = teleport $ Robot (x + fst bounds, y) s
+  | y < 0           = teleport $ Robot (x, y + snd bounds) s
+  | x >= fst bounds = teleport $ Robot (x - fst bounds, y) s
+  | y >= snd bounds = teleport $ Robot (x, y - snd bounds) s
+  | otherwise       = r
+
+step :: Robot -> Robot
+step (Robot (x, y) (sx, sy)) = Robot (x + sx, y + sy) (sx, sy)
+
+mapPos :: [Robot] -> [(Int, Int)]
+mapPos = map (\(Robot p _) -> p)
+
+printR :: [Robot] -> IO ()
+printR rs = putStrLn (robotsToString (mapPos rs))
+
+robotsToString :: [(Int, Int)] -> String
+robotsToString rs = line 0 0
+ where
+  line x y | y == snd bounds  = []
+           | x == fst bounds  = '\n' : line 0 (y + 1)
+           | (x, y) `elem` rs = '#' : line (x + 1) y
+           | otherwise        = '.' : line (x + 1) y
+
+bounds :: (Int, Int)
+bounds = (101, 103)
+
+parse :: String -> Robot
+parse s =
+  let [x, y, sx, sy] = map read . filter (not . null) . splitOneOf "pv=," $ s
+  in  Robot (x, y) (sx, sy)
--- 2024/code/Main.hs
+++ 2024/code/Main.hs
@@ -2,20 +2,21 @@
 
 module Main where
 
-import Day01 as D1
-import Day02 as D2
-import Day04 as D4
-import Day05 as D5
-import Day06 as D6
-import Day07 as D7
-import Day08 as D8
-import Day09 as D9
-import Day10 as D10
-import Day11 as D11
-import Day12 as D12
-import Day13 as D13
-import System.Environment (getArgs)
-import System.Process (callCommand)
+import           Day01                         as D1
+import           Day02                         as D2
+import           Day04                         as D4
+import           Day05                         as D5
+import           Day06                         as D6
+import           Day07                         as D7
+import           Day08                         as D8
+import           Day09                         as D9
+import           Day10                         as D10
+import           Day11                         as D11
+import           Day12                         as D12
+import           Day13                         as D13
+import           Day14                         as D14
+import           System.Environment             ( getArgs )
+import           System.Process                 ( callCommand )
 
 -- Usage:
 -- > cabal run AdventOfCode2024 -- <day>
@@ -26,24 +27,24 @@ main = getArgs >>= runDay
 runDay :: [String] -> IO ()
 runDay args = do
   case args of
-    ["1"] -> D1.main
-    ["2"] -> D2.main
-    ["3"] -> putStrLn "Day 3 was done with Vim keystrokes"
-    ["4"] -> D4.main
-    ["5"] -> D5.main
-    ["6"] -> D6.main
-    ["7"] -> D7.main
-    ["8"] -> D8.main
-    ["9"] -> D9.main >> callCommand "dotnet run --project code -- 9"
+    ["1" ] -> D1.main
+    ["2" ] -> D2.main
+    ["3" ] -> putStrLn "Day 3 was done with Vim keystrokes"
+    ["4" ] -> D4.main
+    ["5" ] -> D5.main
+    ["6" ] -> D6.main
+    ["7" ] -> D7.main
+    ["8" ] -> D8.main
+    ["9" ] -> D9.main >> callCommand "dotnet run --project code -- 9"
     ["10"] -> D10.main
     ["11"] -> D11.main
     ["12"] -> D12.main
     ["13"] -> D13.main
-    [] -> do
+    ["14"] -> D14.main
+    []     -> do
       putStrLn "Enter day to run"
       s <- getLine
       runDay [s]
-    (d : _) ->
-      if read d `elem` [1 .. 25]
-        then putStrLn "Day not implemented yet"
-        else putStrLn "Invalid argument"
+    (d : _) -> if read d `elem` [1 .. 25]
+      then putStrLn "Day not implemented yet"
+      else putStrLn "Invalid argument"
