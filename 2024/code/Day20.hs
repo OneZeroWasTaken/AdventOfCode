09e856444b68c3e61e10d5a1c82607379021c3a5
Erik Eriksson <erik.a.eriksson@sogeti.com>
2024-12-20 11:29:50 +0100

    Day 20 cleanup

--- 2024/code/Day20.hs
+++ 2024/code/Day20.hs
@@ -12,26 +12,21 @@ main :: IO ()
 main = do
   ss <- lines <$> readFile "inputs/input20.txt"
 
-  let a = zip (reverse $ dijk ss) [0 ..]
+  let path = zip (reverse $ dijk ss) [0 ..]
 
-  putStrLn $ "Part 1: " ++ show (length . concatMap (filter (>= 100) . tryCheat a) $ a)
-  putStrLn $ "Part 2: " ++ show (length . concatMap (filter (>= 100) . tryCheatBig a) $ a)
+  putStrLn $ "Part 1: " ++ show (length . allSaved 2 $ path)
+  putStrLn $ "Part 2: " ++ show (length . allSaved 20 $ path)
 
-tryCheatBig :: [(Pos, Int)] -> (Pos, Int) -> [Int]
-tryCheatBig path (p, n) = cheats
+allSaved :: Int -> [(Pos, Int)] -> [Int]
+allSaved d path = concatMap (filter (>= 100) . tryCheat d path) path
+
+tryCheat :: Int -> [(Pos, Int)] -> (Pos, Int) -> [Int]
+tryCheat d path (p, n) = filter (> 0) $ map (\p' -> savedPico (manhattan p p') n $ fromJust $ lookup p' path) cheats
   where
-    ps = map fst path
-    potCheats = filter ((<= 20) . manhattan p) ps
-    cheats = filter (> 0) $ map (\p' -> savedPico (manhattan p p') n $ fromJust $ lookup p' path) potCheats
+    cheats = filter ((<= d) . manhattan p) $ map fst path
 
 manhattan :: Pos -> Pos -> Int
 manhattan (x, y) (x', y') = abs (x - x') + abs (y - y')
-
-tryCheat :: [(Pos, Int)] -> (Pos, Int) -> [Int]
-tryCheat path (p, n) = filter (> 0) $ map (\p' -> savedPico 2 n $ fromJust $ lookup p' path) cheats
-  where
-    ps = map fst path
-    cheats = map fst $ filter (\(p', q) -> p' `elem` ps && q `notElem` ps) $ zip (neighborsDelta 2 p) (neighbors p)
 
 savedPico :: Int -> Int -> Int -> Int
 savedPico d n m
@@ -44,10 +39,7 @@ dijk ss = (s :) . snd . fromJust . dijkstra (filter (`elem` path) . neighbors) (
     path@(s : e : _) = findCoords 'S' ss ++ findCoords 'E' ss ++ findCoords '.' ss
 
 neighbors :: Pos -> [Pos]
-neighbors = neighborsDelta 1
-
-neighborsDelta :: Int -> Pos -> [Pos]
-neighborsDelta d (x, y) = [(x + d, y), (x - d, y), (x, y + d), (x, y - d)]
+neighbors (x, y) = [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]
 
 findCoords :: (Eq a) => a -> [[a]] -> [Pos]
 findCoords i = map fst . concatMap (filter ((== i) . snd) . (\(y, l) -> zipWith (\x c -> ((x, y), c)) [0 ..] l)) . zip [0 ..]
