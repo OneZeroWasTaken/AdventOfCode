42ab0f618294bfc25a59f8bf2bb7f7eff3d4657a
Erik Eriksson <erik.g.eriksson1@gmail.com>
2025-12-01 07:58:06 +0100

    Day 1 part 1

--- 2025/code/Day01.hs
+++ 2025/code/Day01.hs
@@ -4,4 +4,15 @@ import           Data.List
 
 main :: IO ()
 main = do
-  putStrLn $ "Part 1: "
+  l <- lines <$> readFile "inputs/input01.txt"
+  putStrLn $ "Part 1: " ++ show (snd . atZero . map parse $ l)
+
+atZero :: [Int] -> (Int, Int)
+atZero ns = foldl (\(cur, z) n -> next cur z n) (50, 0) ns
+  where
+    next cur z n | (cur + n) `mod` 100 == 0 = (cur + n, z + 1)
+                 | otherwise = (cur + n, z)
+
+parse :: String -> Int
+parse ('L':cs) = - read cs
+parse ('R':cs) = read cs
