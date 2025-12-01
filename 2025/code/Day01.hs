53d4dab3ac195ba6448c4ff0af5e7b454c186c7f
Erik Eriksson <erik.a.eriksson@sogeti.com>
2025-12-01 17:28:52 +0100

    Day 1 part 2

--- 2025/code/Day01.hs
+++ 2025/code/Day01.hs
@@ -1,18 +1,24 @@
 module Day01 where
 
-import           Data.List
+import Data.List
 
 main :: IO ()
 main = do
   l <- lines <$> readFile "inputs/input01.txt"
   putStrLn $ "Part 1: " ++ show (snd . atZero . map parse $ l)
+  putStrLn $ "Part 2: " ++ show (snd . atZero . concatMap parse2 $ l)
 
 atZero :: [Int] -> (Int, Int)
-atZero ns = foldl (\(cur, z) n -> next cur z n) (50, 0) ns
+atZero ns = foldl next (50, 0) ns
   where
-    next cur z n | (cur + n) `mod` 100 == 0 = (cur + n, z + 1)
-                 | otherwise = (cur + n, z)
+    next (cur, z) n
+      | (cur + n) `mod` 100 == 0 = (cur + n, z + 1)
+      | otherwise = (cur + n, z)
+
+parse2 :: String -> [Int]
+parse2 ('L' : cs) = replicate (read cs) (-1)
+parse2 ('R' : cs) = replicate (read cs) 1
 
 parse :: String -> Int
-parse ('L':cs) = - read cs
-parse ('R':cs) = read cs
+parse ('L' : cs) = -read cs
+parse ('R' : cs) = read cs
