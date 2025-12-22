8ac4792083cb77d1f597b509119cea53ddde940c
Erik <erik.g.eriksson1@gmail.com>
2025-12-22 14:00:19 +0100

    Day 10 part 2 attempt

--- 2025/code/Day10.hs
+++ 2025/code/Day10.hs
@@ -1,9 +1,9 @@
 module Day10 where
 
-import Algorithm.Search
-import Data.List
-import Data.List.Split
-import Data.Maybe
+import           Algorithm.Search
+import           Data.List
+import           Data.List.Split
+import           Data.Maybe
 
 main :: IO ()
 main = do
@@ -11,31 +11,47 @@ main = do
 
   let ms = map parse l
   putStrLn $ "Part 1: " ++ show (sum $ map (fst . fromJust . dijk) ms)
-  putStrLn $ "Part 2: " ++ show (map (fst . fromJust . dijk2) (take 3 ms))
+  let (Manual _ f j) = ms !! 0
+  putStrLn $ "Part 2: " ++ show (comb f j)
 
-dijk2 :: Manual -> Maybe (Int, [[Int]])
-dijk2 (Manual _ f c) = dijkstra next (const . const 1) (== c) (replicate (length c) 0)
-  where
-    next :: [Int] -> [[Int]]
-    next state = filter (all (uncurry (<=)) . zip state) $ map (zipWith up state) f
-
-up :: Int -> Bool -> Int
-up n True = n + 1
-up n False = n
+comb :: [[Bool]] -> [Int] -> Maybe Int
+comb f' j' = comb' (sortOn (negate . length . filter id) f') j'
+ where
+  comb' :: [[Bool]] -> [Int] -> Maybe Int
+  comb' _ j | all (== 0) j = Just 0
+  comb' []       j         = Nothing
+  comb' (f : fs) j         = try j maxPresses
+   where
+    active     = zip f j
+    maxPresses = minimum $ map snd $ filter fst active
+    try :: [Int] -> Int -> Maybe Int
+    try jj _ | all (== 0) jj = Just 0
+    try _  0                 = comb' fs j
+    try jj presses           = case comb' fs newJ of
+      Just v  -> Just (v + presses)
+      Nothing -> try jj (presses - 1)
+     where
+      newJ =
+        map (\(isOn, jolts) -> if isOn then jolts - presses else jolts) active
 
 dijk :: Manual -> Maybe (Int, [[Bool]])
-dijk (Manual b f _) = dijkstra next (const . const 1) (== b) (replicate (length b) False)
-  where
-    next :: [Bool] -> [[Bool]]
-    next b' = map (zipWith (/=) b') f
+dijk (Manual b f _) = dijkstra next
+                               (const . const 1)
+                               (== b)
+                               (replicate (length b) False)
+ where
+  next :: [Bool] -> [[Bool]]
+  next b' = map (zipWith (/=) b') f
 
 data Manual = Manual [Bool] [[Bool]] [Int]
   deriving (Eq, Show)
 
 parse :: String -> Manual
 parse s = Manual lights switches joltage
-  where
-    (ls : es) = map (init . tail) $ splitOn " " s
-    lights = map (== '#') ls
-    switches = map (\ns -> map (\i -> i `elem` ns) (take (length ls) [0 ..])) $ map (map read . splitOn ",") (init es)
-    joltage = (map read . splitOn ",") (last es)
+ where
+  (ls : es) = map (init . tail) $ splitOn " " s
+  lights    = map (== '#') ls
+  switches =
+    map (\ns -> map (\i -> i `elem` ns) (take (length ls) [0 ..]))
+      $ map (map read . splitOn ",") (init es)
+  joltage = (map read . splitOn ",") (last es)
