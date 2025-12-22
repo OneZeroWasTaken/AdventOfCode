695c4cb71d1177cad9ba934379d0641c47919ecd
Erik <erik.g.eriksson1@gmail.com>
2025-12-22 18:12:47 +0100

    Finished 2025 (day 10 part 2)

--- 2025/code/Day10.hs
+++ 2025/code/Day10.hs
@@ -11,8 +11,29 @@ main = do
 
   let ms = map parse l
   putStrLn $ "Part 1: " ++ show (sum $ map (fst . fromJust . dijk) ms)
-  let (Manual _ f j) = ms !! 0
-  putStrLn $ "Part 2: " ++ show (comb f j)
+  putStrLn $ "Part 2: " ++ show (sum $ mapMaybe id $ map problem ms)
+
+problem :: Manual -> Maybe Int
+problem (Manual _ f j) = solver (map (map fromEnum) f) j
+
+solver :: [[Int]] -> [Int] -> Maybe Int
+solver _ j | all (== 0) j = Just 0
+           | any (< 0) j  = Nothing
+solver f j | null par  = Nothing
+           | null c    = Nothing
+           | otherwise = Just $ minimum c
+ where
+  par     = parity f j
+  presses = map length par
+  a       = map (foldl (zipWith (+)) (replicate (length j) 0)) par
+  b       = map (map (`div` 2) . zipWith (-) j . (++ repeat 0)) a
+  c       = mapMaybe (\(n, jj) -> (+ n) . (* 2) <$> solver f jj) $ zip presses b
+
+parity :: [[Int]] -> [Int] -> [[[Int]]]
+parity f j = filter
+  ((== target) . map odd . foldl (zipWith (+)) (replicate (length j) 0))
+  (subsequences f)
+  where target = map odd j
 
 comb :: [[Bool]] -> [Int] -> Maybe Int
 comb f' j' = comb' (sortOn (negate . length . filter id) f') j'
